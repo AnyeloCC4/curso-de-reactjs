cd1a4c2b0a81addbb5cd91002719c6c6a0d9b588
Anyelo Cruz <anyelo.cruz.c@gmail.com>
2021-03-31 20:48:35 -0300

    agregado api fake

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,13 +11,20 @@ const App = () => (
    <div className="App">
       <Header />
       <Search />
-      <Categories>
+      <Categories title="Mi Lista">
          <Carousel>
             <CarouselItem />
+         </Carousel>
+      </Categories>
+
+      <Categories title="Tendencias">
+         <Carousel>
             <CarouselItem />
-            <CarouselItem />
-            <CarouselItem />
-            <CarouselItem />
+         </Carousel>
+      </Categories>
+
+      <Categories title="Originales de Platzi Video">
+         <Carousel>
             <CarouselItem />
          </Carousel>
       </Categories>
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
    entry: './src/index.js',
    output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
+      assetModuleFilename: 'assets/[hash].[ext]'
    },
    resolve: {
       extensions: ['.js', '.jsx']
@@ -38,6 +39,15 @@ module.exports = {
                'sass-loader',
                
             ]
+         },
+         {
+            test: /\.(png|gif|jpg)$/,
+            use: [
+               {
+                 loader: 'file-loader',
+                 options: { name: 'assets/[hash].[ext]' },
+               }
+             ],
          }
       ]
    },
